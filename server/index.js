ff48af710ee7d510ff8130bade46a21a01d272b6
ptdat46 <ptdat46@gmail.com>
2024-07-07 20:20:36 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express()
+const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello from MERN stack!');
+  res.send('Hello from MERN stack!')
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}.`);
-});
+})
